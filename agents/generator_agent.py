b11a76b17d94886262a82c38c26be1131ae756b1
Dhiraj Shah <dhirajssh@gmail.com>
2025-09-21 19:02:38 -0700

    feat: generator_agent code added. generator.py

---
+++ agents/generator_agent.py
@@ -0,0 +1,28 @@
+import os
+from pydantic_ai import Agent
+from pydantic_ai.models.google import GoogleModel
+from pydantic_ai.providers.google import GoogleProvider
+from utils import load_system_prompt
+from dotenv import load_dotenv
+import streamlit as st
+
+def initialize_generator_agent():
+  load_dotenv()
+  api_key = os.getenv("GOOGLE_API_KEY")
+  provider = GoogleProvider(api_key=api_key)
+  model = GoogleModel("gemini-2.5-pro", provider=provider)
+  resume_path = os.path.join(os.path.dirname(__file__), "..", "resume.md")
+  if os.path.exists(resume_path):
+    resume = load_system_prompt("resume.md")
+    st.session_state.resume = resume
+  else:
+    resume = st.session_state.resume
+
+  prompt = load_system_prompt("generator.md")
+  full_prompt = f"{prompt}\n\n---\n\n{resume}"
+
+  agent = Agent(
+    model = model,
+    instructions = full_prompt
+  )
+  return agent
