6903c4d754e70e3c441f68d3e383825f57bddb4b
Dhiraj Shah <dhirajssh@gmail.com>
2025-09-21 17:59:34 -0700

    feat: the orchestrator agent works. app.py

---
+++ agents/orchestrator_agent.py
@@ -0,0 +1,75 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.google import GoogleModel
+from pydantic_ai.providers.google import GoogleProvider
+from pydantic import Field, field_validator, BaseModel
+from typing import Optional, Literal
+import os
+from utils import load_system_prompt
+from dotenv import load_dotenv
+
+class OrchestratorResponse(BaseModel):
+  """Structured response for the orchestrator agent."""
+  agent:Literal["summarizer_agent", "generator_agent"] = Field(
+    ...,
+    description="""
+    Specifies which agent should handle the request.
+
+    - "summarizer_agent": Use when the user provides a job URL and the job description needs to be fetched and summarized.
+    - "generator_agent": Use when no URL is provided. This applies to cover letter requests, answering application questions, or any other scenario without a job link.
+    """,
+    examples=[
+      "summarizer_agent",
+      "generator_agent",
+    ]
+  )
+  link: Optional[str] = Field(
+    default=None,
+    description="""
+      The job application URL provided by the user.
+      This is required when `agent` is "summarizer_agent".
+      If no URL is provided by the user, this should remain None.
+    """,
+    examples=[
+      "https://boards.greenhouse.io/company/jobs/1234567",
+    ]
+  )
+  message: Optional[str] = Field(
+    default=None,
+    description="""
+      The original user message.
+      Required when `agent` is "generator_agent".
+      Otherwise, can be None.
+    """,
+    examples=[
+      "I need a cover letter for the software engineer position.",
+      "Why am I a good fit for this role?",
+    ]
+  )
+
+  @field_validator('agent')
+  @classmethod
+  def check_agent(cls, value:str)->str:
+    field_info = cls.model_fields["agent"]
+    description = field_info.description
+    examples = set(field_info.examples)
+
+    if value not in examples:
+      raise ValueError(
+        f"The value of this field should be one of 2: {examples}."
+        f"\n\nWas provided invalid value: {value}"
+        f"\n\nField Description: {description}"
+      )
+    return value
+  
+def initialize_orchestrator_agent():
+  load_dotenv()
+  api_key = os.getenv("GOOGLE_API_KEY")
+  provider = GoogleProvider(api_key=api_key)
+  model = GoogleModel("gemini-2.5-pro", provider=provider)
+  agent = Agent(
+    model=model,
+    instructions=load_system_prompt("orchestrator.md"),
+    output_retries=5,
+    output_type=OrchestratorResponse
+  )
+  return agent
---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from agents.orchestrator_agent import initialize_orchestrator_agent
+
+def initialize_session_state():
+  st.session_state["messages"] = []
+  st.session_state.orchestrator_agent = initialize_orchestrator_agent()
+
+# Callback functions described here
+def chat_input_callback():
+  st.session_state["messages"].append(
+    {"role": "user", "content": st.session_state["user_input"]}
+  )
+  display_messages()
+
+  with st.status("Generating Summary...", expanded=False) as status:
+    try:
+      result = st.session_state.orchestrator_agent.run_sync(
+        user_prompt = st.session_state["user_input"],
+        message_history = st.session_state["messages"],
+      )
+      print(result.output)
+      st.session_state["messages"].append(
+        {"role": "assistant", "content": result.output}
+      )
+      st.markdown(result.output)
+      status.update(label="✅ Summary complete", state="complete")
+    except Exception as e:
+      error_msg = f"❌ Error: {e}"
+      st.session_state["messages"].append(
+          {"role": "assistant", "content": error_msg}
+      )
+      st.error(error_msg)
+      st.session_state["messages"].append(
+        {"role": "assistant", "content": error_msg}
+      )
+      status.update(label="❌ Failed to generate summary", state="error")
+
+
+if "messages" not in st.session_state:
+  initialize_session_state()
+
+def display_messages():
+  for msg in st.session_state["messages"]:
+    if msg["role"] == "assistant":
+      with st.chat_message(msg["role"]):
+        with st.status("✅ Summary complete", expanded=False, state="complete"):
+          st.markdown(msg["content"])
+    else:
+      with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+
+st.chat_input("Paste a job URL", key="user_input", on_submit=chat_input_callback)
