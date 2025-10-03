2811d9b040ff9a8d6616fb7968036df25e59da47
Dhiraj Shah <dhirajssh@gmail.com>
2025-10-03 07:33:46 -0700

    chore: resume.md was having a problem while reading it. generator_agent.py

--- agents/generator_agent.py
+++ agents/generator_agent.py
@@ -8,16 +8,12 @@ import streamlit as st
 
 def initialize_generator_agent():
   load_dotenv()
-  api_key = os.getenv("GOOGLE_API_KEY")
-  provider = GoogleProvider(api_key=api_key)
-  model = GoogleModel("gemini-2.5-pro", provider=provider)
-  resume_path = os.path.join(os.path.dirname(__file__), "..", "resume.md")
+  resume_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resume.md"))
   if os.path.exists(resume_path):
-    resume = load_system_prompt("resume.md")
+    resume = load_system_prompt("resume.md", flag=False)
     st.session_state.resume = resume
   else:
     resume = st.session_state.resume
-
   prompt = load_system_prompt("generator.md")
   full_prompt = f"{prompt}\n\n---\n\n{resume}"
 
